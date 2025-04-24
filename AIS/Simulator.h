beafcc41893a7fa3531636b961c3d24e7df47fb7
hlevil <90902828+hlevil@users.noreply.github.com>
2025-04-24 20:44:55 +0300

    Update Simulator.h

--- AIS/Simulator.h
+++ AIS/Simulator.h
@@ -5,30 +5,31 @@
 class Simulator
 {
 public:
-	class SimulatorOptions
-	{
-	public:
-		size_t population_size;
-		size_t dimensions;
-		size_t generations;
-		double mutation_rate;
-		double shrick_rate;
-		std::pair<double, double> search_area;
-		SimulatorOptions(size_t population_size = 1000, size_t dimensions = 2, size_t generations = 1000,
-			double mutation_rate = 5, double shrick_rate = 100, std::pair<double, double> search_area = { -20, 20 })
-			: population_size(population_size), dimensions(dimensions), generations(generations),
-			mutation_rate(mutation_rate), shrick_rate(shrick_rate), search_area(search_area) {
-		};
-	};
+    class SimulatorOptions
+    {
+    public:
+        size_t population_size;
+        size_t dimensions;
+        size_t generations;
+        double mutation_rate;
+        double shrick_rate;
+        std::pair<double, double> search_area;
+        SimulatorOptions(size_t population_size = 1000, size_t dimensions = 2, size_t generations = 1000,
+            double mutation_rate = 5, double shrick_rate = 100, std::pair<double, double> search_area = { -20, 20 })
+            : population_size(population_size), dimensions(dimensions), generations(generations),
+            mutation_rate(mutation_rate), shrick_rate(shrick_rate), search_area(search_area) {
+        };
+    };
 
-	// Алгоритм клональной селекции
-	void operator()(SimulatorOptions opt);
+    void operator()(SimulatorOptions opt);
+    
+    // Р”РѕР±Р°РІРёР»Р° РЅРѕРІС‹Р№ РјРµС‚РѕРґ, С‡С‚РѕР±С‹ СЌС‚Рѕ РІСЃРµ С‚РµСЃС‚РёС‚СЊ :))))
+    const std::vector<Antibody>& getPopulation() const { return population; }
 
 private:
-	std::vector<Antibody> population;
+    std::vector<Antibody> population;
 
-	void printBest(size_t gen, size_t dimensions);
-
-	void printResult(size_t dimensions);
+    void printBest(size_t gen, size_t dimensions);
+    void printResult(size_t dimensions);
 };
 
