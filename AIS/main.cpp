f69b8a63868e114c975e609971b1ce92a19b29c6
FakeAccLol <bastordon@gmail.com>
2025-05-29 16:22:42 +0300

    е

--- AIS/main.cpp
+++ AIS/main.cpp
@@ -2,9 +2,14 @@
 #include "myutils.h"
 #include <iostream>
 #include <memory>
+#include <fstream>;
 #include <vector>
 
+using std::cout;
+using std::endl;
+
 int main() {
+    std::ofstream out("tests.csv");
     // Создаем объекты разных реализаций
     Options def;
     std::vector<std::unique_ptr<AIS>> methods;
@@ -23,6 +28,95 @@ int main() {
         testing::Return rez3 = testing::testGenAmount(std::move(method));
         testing::Return rez4 = testing::testMutationRate(std::move(method));
         testing::Return rez5 = testing::testShrinkRate(std::move(method));
+        out << "Basic" << endl;
+        out << "Time" << endl;
+        for (auto& val : rez1.timeStamps)
+            out << val << ';';
+        out << endl;
+        out << "Calls" << endl;
+        for (auto& val : rez1.calls)
+            out << val << ';';
+        out << endl;
+        out << "process" << endl;
+        for (int i = 0; i < rez1.info.size(); ++i) {
+            out << i << endl;
+            for (auto& pai : rez1.info[i])
+                out << pai.first << ';' << pai.second << endl;
+            out << endl;
+        }
+        out << endl;
+
+        out << "Basic" << endl;
+        out << "Time" << endl;
+        for (auto& val : rez2.timeStamps)
+            out << val << ';';
+        out << endl;
+        out << "Calls" << endl;
+        for (auto& val : rez2.calls)
+            out << val << ';';
+        out << endl;
+        out << "process" << endl;
+        for (int i = 0; i < rez2.info.size(); ++i) {
+            out << i << endl;
+            for (auto& pai : rez2.info[i])
+                out << pai.first << ';' << pai.second << endl;
+            out << endl;
+        }
+        out << endl;
+
+        out << "Basic" << endl;
+        out << "Time" << endl;
+        for (auto& val : rez3.timeStamps)
+            out << val << ';';
+        out << endl;
+        out << "Calls" << endl;
+        for (auto& val : rez3.calls)
+            out << val << ';';
+        out << endl;
+        out << "process" << endl;
+        for (int i = 0; i < rez3.info.size(); ++i) {
+            out << i << endl;
+            for (auto& pai : rez3.info[i])
+                out << pai.first << ';' << pai.second << endl;
+            out << endl;
+        }
+        out << endl;
+
+        out << "Basic" << endl;
+        out << "Time" << endl;
+        for (auto& val : rez4.timeStamps)
+            out << val << ';';
+        out << endl;
+        out << "Calls" << endl;
+        for (auto& val : rez4.calls)
+            out << val << ';';
+        out << endl;
+        out << "process" << endl;
+        for (int i = 0; i < rez4.info.size(); ++i) {
+            out << i << endl;
+            for (auto& pai : rez4.info[i])
+                out << pai.first << ';' << pai.second << endl;
+            out << endl;
+        }
+        out << endl;
+
+        out << "Basic" << endl;
+        out << "Time" << endl;
+        for (auto& val : rez5.timeStamps)
+            out << val << ';';
+        out << endl;
+        out << "Calls" << endl;
+        for (auto& val : rez5.calls)
+            out << val << ';';
+        out << endl;
+        out << "process" << endl;
+        for (int i = 0; i < rez5.info.size(); ++i) {
+            out << i << endl;
+            for (auto& pai : rez5.info[i])
+                out << pai.first << ';' << pai.second << endl;
+            out << endl;
+        }
+        out << endl;
     }
     
     return 0;
