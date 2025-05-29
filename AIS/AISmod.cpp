9559dcd73c2080492dce7e12c1a31ea9bcbf133d
FakeAccLol <bastordon@gmail.com>
2025-05-29 14:52:39 +0300

    fixed methods

--- AIS/AISmod.cpp
+++ AIS/AISmod.cpp
@@ -7,7 +7,7 @@ AISmod::AISmod(Options opt) : AIS(opt)
 
 void AISmod::run()
 {
-	/*// Initial population
+	// Initial population
 	for (size_t i = 0; i < params.population_size; ++i)
 		population.emplace_back(std::make_unique<Cellmod>(params.dimensions, foo, params.search_area));
 
@@ -22,16 +22,18 @@ void AISmod::run()
 		std::vector<std::unique_ptr<Cell>> new_population;
 		size_t elite_size = params.population_size / params.shrick_rate;
 		for (size_t i = 0; i < elite_size; ++i)
-			new_population.push_back(population[i]);
+			new_population.push_back(std::move(population[i]));
 
 		// Mutate others
 		for (size_t i = elite_size; i < params.population_size; ++i) {
 			population[i]->mutate(params.mutation_rate);
-			new_population.push_back(population[i]);
+			new_population.push_back(std::move(population[i]));
 			++calls;
 		}
 
 		// move them
 		population = std::move(new_population);
-	}*/
+
+		info.push_back({ gen, population[0]->affinity });
+	}
 }
--- AIS/Cell.cpp
+++ AIS/Cell.cpp
@@ -1,7 +1,7 @@
 #include "Cell.h"
 
 Cell::Cell(size_t dim, function<double(vector<double> x)> f, vector<pair<double, double>> bounds) : position(dim), gen(std::random_device()()), affinity(std::numeric_limits<double>::infinity()), foo(f), bounds(bounds) {
-	if (bounds.size() != dim - 1)
+	if (bounds.size() != dim)
 		throw 53;
 	for (size_t i = 0; i < dim; ++i) {
 		std::uniform_real_distribution<> dis(bounds[i].first, bounds[i].second);
